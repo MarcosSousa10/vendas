3e84b8a5d7b5c0d0f8eac242bf3e9fe5db69203f
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-01-20 11:23:26 -0300

    20012023

--- src/main/java/com/vendas/vendas/rest/produtos/ProdutoController.java
+++ src/main/java/com/vendas/vendas/rest/produtos/ProdutoController.java
@@ -1,10 +1,18 @@
 package com.vendas.vendas.rest.produtos;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.vendas.vendas.model.Produto;
 import com.vendas.vendas.model.repository.ProdutoRepository;
@@ -21,5 +29,18 @@ public class ProdutoController {
         repository.save(entidadeProduto);
 
         return ProdutoFormRequest.fromModel(entidadeProduto);
+    }
+    //void esta retornando um responseentity sem corpo
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto){
+        Optional<Produto> produtoExistente=repository.findById(id);
+        if(produtoExistente.isEmpty()){
+       //public void =>     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+         return ResponseEntity.notFound().build();
+        }
+        Produto entidade=produto.toModel();
+        entidade.setId(id);
+        repository.save(entidade);
+        return ResponseEntity.ok().build();
     }
 }
