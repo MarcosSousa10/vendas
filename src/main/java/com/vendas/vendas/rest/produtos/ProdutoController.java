848f7a9ad5cc13a028731bdd31c28932ecfdd413
marcos sousa <marcospegodesousa10@gmail.com>
2023-01-21 14:26:06 -0300

    21

--- src/main/java/com/vendas/vendas/rest/produtos/ProdutoController.java
+++ src/main/java/com/vendas/vendas/rest/produtos/ProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,5 +67,15 @@ public class ProdutoController {
         entidade.setId(id);
         repository.save(entidade);
         return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        Optional<Produto> produtoExistente=repository.findById(id);
+        if(produtoExistente.isEmpty()){
+            return ResponseEntity.notFound().build(); 
+        }
+        repository.delete((produtoExistente.get()));
+        return ResponseEntity.noContent().build();
+
     }
 }
