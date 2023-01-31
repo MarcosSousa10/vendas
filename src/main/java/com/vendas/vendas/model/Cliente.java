431e6ed44e05269fe8bfcf77b8f77e09f83604c7
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-01-31 14:02:30 -0300

    31012023

--- src/main/java/com/vendas/vendas/model/Cliente.java
+++ src/main/java/com/vendas/vendas/model/Cliente.java
@@ -2,7 +2,6 @@ package com.vendas.vendas.model;
 
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
