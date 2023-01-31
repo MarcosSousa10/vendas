6b916a6bf4ef0edae4a3b8684dfb96739491dc1f
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-01-31 16:14:16 -0300

    31012023

--- src/main/java/com/vendas/vendas/model/repository/ClienteRepository.java
+++ src/main/java/com/vendas/vendas/model/repository/ClienteRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import com.vendas.vendas.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-    @Query("select c from Cliente c where upper(c.nome) like :nome and c.cpf like :cpf")
+    @Query("select c from Cliente c where upper(c.nome) like upper(:nome) and c.cpf like :cpf")
     Page<Cliente> buscarPorNomeCpf(@Param("nome") String nome,@Param("cpf") String cpf, Pageable pageable);
 }
